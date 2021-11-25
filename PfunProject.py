ca8d943649e17d46c43719d566b3ee1d80f13c9d
zainabhaiderr <95019642+zainabhaiderr@users.noreply.github.com>
2021-11-25 14:17:54 +0500

    Update PfunProject.py

--- PfunProject.py
+++ PfunProject.py
@@ -51,6 +51,11 @@ def input_typeOfRoom():
             typeOfRoom = input("Error! Try again:")
 
     print(typeOfRoom)    
+def input_arrivalDate():
+    date=input("Please enter your date of arrival in the format dd/mm/yyyy:")
+    
+def input_departureDate():
+    pass    
 def input_durationOfStay():
     durationOfStay=input("Enter the number of nights you wish to stay:")
     b=verifyInput_DurationOfStay(durationOfStay)
@@ -59,6 +64,7 @@ def input_durationOfStay():
         b=verifyInput_DurationOfStay(durationOfStay)
     durationOfStay=int(durationOfStay)    
     print(durationOfStay)
+
 def verifyInput_DurationOfStay(durationOfStay):                     #verification of user input; number of children
     b=True
     for i in durationOfStay:
@@ -68,21 +74,50 @@ def verifyInput_DurationOfStay(durationOfStay):                     #verificatio
             b=False
             break
     return b    
-def facilities():
-    pass
+def breakfast():
+    choice=input("Would you like to include our breakfast facility during your stay? Enter 'y' for yes and 'n' for no:")
+    choice=choice.lower()
+    while True:
+        if choice =="y" or choice=="n":
+            break
+        else:
+            choice=input("Error! Try agian. Enter 'y' for yes and 'n' for no:")
+    if choice=="y":
+        return True
+    else:
+        return False
+def swimming_gym():
+    choice=input("Would you like to avail our swimming pool and gym facility during your stay? Enter 'y' for yes and 'n' for no:")
+    choice=choice.lower()
+    while True:
+        if choice =="y" or choice=="n":
+            break
+        else:
+            choice=input("Error! Try agian. Enter 'y' for yes and 'n' for no:")
+    if choice=="y":
+        return True
+    else:
+        return False    
+    
 
 singleRoom_perNight = 200.0         #prices in USD
 doubleRoom_perNight = 300.0
 familyRoom_perNight = 450.0
 suiteRoom_perNight = 600.0
-breakfast = 100.0
-swimmingPool_Gym = 75.0
+breakfast_charges = 100.0
+swimmingPool_Gym_charges = 75.0
 winter_discount = 15.0
 summer_discount = 25.0
 familyPackage_discount = 20.0
 couplePackage_discount = 15.0
-input_name()
-input_numberOfAdults()
-input_numberOfChildren()
-input_typeOfRoom()
-input_durationOfStay()
+name=input_name()
+adults=input_numberOfAdults()
+children=input_numberOfChildren()
+room=input_typeOfRoom()
+duration=input_durationOfStay()
+if room!="suite":
+    breakfast_included=breakfast()
+    swimming_gym_included=swimming_gym()
+else:
+    breakfast_included=True
+    swimming_gym_inlcluded=True
